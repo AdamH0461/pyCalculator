b017387c5863c65110b45874d01482609c915146
AdamH0461 <hodgkinson.adam@gmail.com>
2017-05-18 16:41:22 +0100

    added tkinter.py for reference on basics

--- tKinter.py
+++ tKinter.py
@@ -1,2 +1,17 @@
 import tkinter
-tkinter
+#creates window
+window = tkinter.Tk()
+#sets window title
+window.title("title")
+#window size
+window.geometry("300x300")
+#creates label, entry box, and button
+label = tkinter.Label(window, text="Label")
+entry = tkinter.Entry(window)
+button = tkinter.Button(window, text="this is button")
+#packs them onto screen
+entry.pack()
+button.pack()
+label.pack()
+#draws screen
+window.mainloop()
